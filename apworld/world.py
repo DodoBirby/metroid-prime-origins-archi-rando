701f97fc8b4144606cb2da2910c396146e00c549
DodoBirby <95009974+DodoBirby@users.noreply.github.com>
2026-08-18 19:57:28 +1000

    Create dummy apworld

---
+++ apworld/world.py
@@ -0,0 +1,9 @@
+from worlds.AutoWorld import World
+
+class MetroidPrimeOriginsWorld(World):
+    """
+    Metroid Prime Origins is a 2d demake of Metroid Prime.
+    """
+    game = "Metroid Prime Origins"
+
+
