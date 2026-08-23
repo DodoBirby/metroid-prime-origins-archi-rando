dc330d75713da02f1da02f5abfb89079d77b6853
DodoBirby <95009974+DodoBirby@users.noreply.github.com>
2026-08-23 18:53:31 +1000

    Fix issues in chozo ruins logic

--- apworld/regions.py
+++ apworld/regions.py
@@ -73,7 +73,6 @@ def create_and_connect_regions(world: MetroidPrimeOriginsWorld):
 
     _ = west_ruins.connect(upper_west_ruins, "West Ruins to Upper West Ruins")
     _ = west_ruins.connect(central_ruins, "West Ruins to Central Ruins")
-    _ = west_ruins.connect(past_ruins_magma_pool, "West Ruins to Past Magma Pool")
     _ = west_ruins.connect(ruined_shrine, "West Ruins to Ruined Shrine")
 
     _ = upper_west_ruins.connect(sun_tower_and_flaahgra, "Upper West Ruins to Flaahgra")
--- apworld/rules.py
+++ apworld/rules.py
@@ -14,9 +14,12 @@ CAN_SUPER_MISSILE = Has("Missile Tank") & Has("Super Missile")
 CAN_BOOST = Has("Morph Ball") & Has("Boost Ball")
 CAN_PB = Has("Morph Ball") & Has("Power Bomb")
 CAN_DESTROY_BOMB_BLOCKS = CAN_BOMB | CAN_PB | Has("Screw Attack")
+CAN_DESTROY_BLOCKS_WHILE_MORPHED = CAN_BOMB | CAN_PB
+CAN_TRAVERSE_UNDERWATER = Has("Gravity Suit") | Has("Grapple Beam")
 
 # Frigate requires shooting conduits, traversing underwater ledges, and destroying bomb blocks + glass to reveal conduits
-CAN_TRAVERSE_FRIGATE = Has("Wave Beam") & CAN_DESTROY_BOMB_BLOCKS & Has("Missile Tank") & (Has("Gravity Suit") | Has("Grapple Beam"))
+CAN_TRAVERSE_FRIGATE = Has("Wave Beam") & CAN_DESTROY_BOMB_BLOCKS & Has("Missile Tank") & CAN_TRAVERSE_UNDERWATER
+
 
 # This rule should be used for overhangs where you can't swwj but you can get through with vertical movement or climbing up the side
 CAN_TRAVERSE_LOW_OVERHANG = Has("Space Jump Boots") | CAN_SPIDER | Has("Grapple Beam") | CAN_IBJ
@@ -52,7 +55,6 @@ def set_region_connection_rules(world: MetroidPrimeOriginsWorld):
     
     set_entrance_rule("West Ruins to Upper West Ruins", Has("Missile Tank") & Has("Morph Ball"), world)
     set_entrance_rule("West Ruins to Central Ruins", Has("Morph Ball"), world)
-    set_entrance_rule("West Ruins to Past Magma Pool", Has("Grapple Beam"), world)
     set_entrance_rule("West Ruins to Ruined Shrine", Has("Missile Tank"), world)
 
     set_entrance_rule("Upper West Ruins to Flaahgra", CAN_SUPER_MISSILE & CAN_TRAVERSE_HIGH_OVERHANG, world)
@@ -66,10 +68,10 @@ def set_region_connection_rules(world: MetroidPrimeOriginsWorld):
     set_entrance_rule("Arboretum to Gathering Hall", Has("Missile Tank"), world)
     set_entrance_rule("Arboretum to Flaahgra", Has("Morph Ball") & CAN_DESTROY_BOMB_BLOCKS & Has("Missile Tank"), world)
 
-    set_entrance_rule("Gathering Hall to Dynamo", Has("Missile Tank") & CAN_DESTROY_BOMB_BLOCKS, world)
+    set_entrance_rule("Gathering Hall to Dynamo", Has("Missile Tank") & Has("Morph Ball") & CAN_DESTROY_BOMB_BLOCKS, world)
     set_entrance_rule("Gathering Hall to Energy Core", Has("Morph Ball"), world)
 
-    set_entrance_rule("Energy Core to Furnace", (Has("Gravity Suit") & CAN_TRAVERSE_LOW_OVERHANG) | Has("Grapple Beam") | CAN_BOMB, world)
+    set_entrance_rule("Energy Core to Furnace", CAN_TRAVERSE_LOW_OVERHANG | CAN_BOMB, world)
 
     set_entrance_rule("Furnace to Upper Furnace", Has("Morph Ball") & CAN_DESTROY_BOMB_BLOCKS & (CAN_SPIDER | Has("Grapple Beam")), world)
 
@@ -80,7 +82,7 @@ def set_region_connection_rules(world: MetroidPrimeOriginsWorld):
 
     set_entrance_rule("Elder Hall to Lower Reflecting Pool", Has("Wave Beam") & CAN_BOMB & CAN_TRAVERSE_LOW_OVERHANG, world)
 
-    set_entrance_rule("Lower Pool to Upper Pool", (CAN_DESTROY_BOMB_BLOCKS & CAN_BOOST) | Has("Grapple Beam"), world)
+    set_entrance_rule("Lower Pool to Upper Pool", (CAN_DESTROY_BOMB_BLOCKS & CAN_BOOST) | Has("Grapple Beam") | CAN_IBJ, world)
 
 
 def set_location_rules(world: MetroidPrimeOriginsWorld):
@@ -106,11 +108,10 @@ def set_location_rules(world: MetroidPrimeOriginsWorld):
 
     # Chozo Ruins
     set_location_rule("(Chozo Ruins) Main Plaza - Super Missile Blocks", CAN_SUPER_MISSILE & CAN_TRAVERSE_LOW_OVERHANG, world)
-    set_location_rule("(Chozo Ruins) Main Plaza - Boost Ball Ramp", CAN_BOOST, world)
-    # Kill the nest and do a low overhang move or just IBJ/grapple
-    set_location_rule("(Chozo Ruins) Main Plaza - Lower Ledge", (Has("Missile Tank") & CAN_TRAVERSE_LOW_OVERHANG) | Has("Grapple Beam") | CAN_IBJ | Has("Enter Main Plaza From Ledge"), world)
+    set_location_rule("(Chozo Ruins) Main Plaza - Boost Ball Ramp", CAN_BOOST | Has("Space Jump Boots"), world)
+    set_location_rule("(Chozo Ruins) Main Plaza - Lower Ledge", CAN_TRAVERSE_LOW_OVERHANG | Has("Enter Main Plaza From Ledge"), world)
     set_location_rule("(Chozo Ruins) Main Plaza - Top Ledge", Has("Grapple Beam") | CAN_IBJ, world)
-    set_location_rule("(Chozo Ruins) Ruined Nursery", CAN_DESTROY_BOMB_BLOCKS & Has("Morph Ball"), world)
+    set_location_rule("(Chozo Ruins) Ruined Nursery", CAN_DESTROY_BLOCKS_WHILE_MORPHED & Has("Morph Ball"), world)
     set_location_rule("(Chozo Ruins) Ruined Gallery - Right Tunnel", Has("Morph Ball"), world)
     set_location_rule("(Chozo Ruins) Ruined Gallery - Left Wall", Has("Missile Tank"), world)
     set_location_rule("(Chozo Ruins) Transport Access North", Has("Missile Tank"), world)
@@ -118,11 +119,11 @@ def set_location_rules(world: MetroidPrimeOriginsWorld):
     set_location_rule("(Chozo Ruins) Vault", CAN_BOMB & Has("Morph Ball"), world)
     set_location_rule("Enter Main Plaza From Ledge", Has("Morph Ball"), world)
 
-    set_location_rule("(Chozo Ruins) Ruined Fountain", CAN_BOOST | CAN_SPIDER, world)
+    set_location_rule("(Chozo Ruins) Ruined Fountain", CAN_BOOST | CAN_SPIDER | CAN_IBJ, world)
 
     set_location_rule("(Chozo Ruins) Watery Hall Access", Has("Missile Tank"), world)
     set_location_rule("(Chozo Ruins) Watery Hall - Top", Has("Missile Tank"), world)
-    set_location_rule("(Chozo Ruins) Watery Hall - Underwater Tunnel", Has("Missile Tank") & Has("Gravity Suit"), world)
+    set_location_rule("(Chozo Ruins) Watery Hall - Underwater Tunnel", Has("Missile Tank") & CAN_TRAVERSE_UNDERWATER, world)
     set_location_rule("(Chozo Ruins) Gathering Hall", Has("Morph Ball") & CAN_DESTROY_BOMB_BLOCKS, world)
 
     set_location_rule("(Chozo Ruins) Dynamo - Bottom", Has("Missile Tank"), world)
@@ -132,15 +133,15 @@ def set_location_rules(world: MetroidPrimeOriginsWorld):
     set_location_rule("(Chozo Ruins) Burn Dome - Back Room", Has("Morph Ball") & CAN_DESTROY_BOMB_BLOCKS, world)
 
     set_location_rule("(Chozo Ruins) Furnace - Chamber", Has("Morph Ball"), world)
-    set_location_rule("(Chozo Ruins) Furnace - Boost Ceiling", CAN_BOOST & CAN_SPIDER & CAN_DESTROY_BOMB_BLOCKS, world)
+    set_location_rule("(Chozo Ruins) Furnace - Boost Ceiling", CAN_BOOST & (CAN_SPIDER | Has("Grapple Beam")) & CAN_DESTROY_BOMB_BLOCKS & CAN_PB, world)
 
-    set_location_rule("(Chozo Ruins) Crossway", CAN_BOOST & CAN_BOMB, world)
+    set_location_rule("(Chozo Ruins) Crossway", Has("Morph Ball") & CAN_BOOST & CAN_BOMB, world)
 
     set_location_rule("(Chozo Ruins) Hall of the Elders - Ceiling", Has("Ice Beam") & CAN_BOMB & CAN_SPIDER, world)
     # TODO: On remix you can use the secret path
     set_location_rule("(Chozo Ruins) Elder Chamber", (Has("Plasma Beam") & CAN_BOMB), world)
 
-    set_location_rule("(Chozo Ruins) Antechamber", Has("Missile Tank") & (CAN_BOMB | CAN_PB), world)
+    set_location_rule("(Chozo Ruins) Antechamber", Has("Missile Tank") & CAN_BOMB, world)
 
     set_location_rule("(Chozo Ruins) Magma Pool", CAN_PB, world)
     set_location_rule("(Chozo Ruins) Training Chamber Access", Has("Morph Ball"), world)
@@ -150,7 +151,7 @@ def set_location_rules(world: MetroidPrimeOriginsWorld):
     set_location_rule("(Chozo Ruins) Ruined Shrine - Tunnel", CAN_DESTROY_BOMB_BLOCKS & Has("Morph Ball"), world)
 
     set_location_rule("(Chozo Ruins) Tower of Light", CAN_SUPER_MISSILE, world)
-    set_location_rule("(Chozo Ruins) Tower Chamber", Has("Gravity Suit") & Has("Wave Beam"), world)
+    set_location_rule("(Chozo Ruins) Tower Chamber", CAN_TRAVERSE_UNDERWATER & Has("Wave Beam"), world)
     # Intentionally not CAN_SPIDER since this is about triggering the event flag
     set_location_rule("(Chozo Ruins) Sunchamber - Ghost Reward", Has("Spider Ball"), world)
 
