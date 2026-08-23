1e55565e60305a1c945ca873cb442074e843c95c
DodoBirby <95009974+DodoBirby@users.noreply.github.com>
2026-08-23 16:10:01 +1000

    Add Chozo Ruins logic

--- apworld/regions.py
+++ apworld/regions.py
@@ -31,6 +31,24 @@ def create_and_connect_regions(world: MetroidPrimeOriginsWorld):
     gated_east_tallon = create_region("(Tallon Overworld) Gated East Tallon", regions, world)
     life_grove = create_region("(Tallon Overworld) Life Grove", regions, world)
 
+    # Chozo Ruins
+    west_ruins = create_region("(Chozo Ruins) West Chozo Ruins", regions, world)
+    upper_west_ruins = create_region("(Chozo Ruins) Upper West Chozo Ruins", regions, world)
+    ruined_shrine = create_region("(Chozo Ruins) Ruined Shrine", regions, world)
+    central_ruins = create_region("(Chozo Ruins) Central Ruins", regions, world)
+    past_ruins_magma_pool = create_region("(Chozo Ruins) Past the Magma Pool", regions, world)
+    arboretum = create_region("(Chozo Ruins) Arboretum", regions, world)
+    gathering_hall = create_region("(Chozo Ruins) Gathering Hall", regions, world)
+    dynamo = create_region("(Chozo Ruins) Dynamo", regions, world)
+    energy_core = create_region("(Chozo Ruins) Energy Core", regions, world)
+    furnace = create_region("(Chozo Ruins) Furnace", regions, world)
+    upper_furnace = create_region("(Chozo Ruins) Upper Furnace", regions, world)
+    crossway = create_region("(Chozo Ruins) Crossway", regions, world)
+    elder_hall = create_region("(Chozo Ruins) Hall of the Elders", regions, world)
+    lower_reflecting_pool = create_region("(Chozo Ruins) Lower Reflecting Pool", regions, world)
+    upper_reflecting_pool = create_region("(Chozo Ruins) Upper Reflecting Pool", regions, world)
+    tower_of_light = create_region("(Chozo Ruins) Tower of Light", regions, world)
+    sun_tower_and_flaahgra = create_region("(Chozo Ruins) Sun Tower and Flaahgra", regions, world)
 
     world.multiworld.regions += regions
 
@@ -39,6 +57,8 @@ def create_and_connect_regions(world: MetroidPrimeOriginsWorld):
     _ = landing_site.connect(artifact_temple)
     _ = landing_site.connect(west_tallon, "Landing Site to West Tallon")
     _ = landing_site.connect(crash_site_left, "Landing Site to Crash Site")
+    # Elevator
+    _ = landing_site.connect(west_ruins)
 
     _ = crash_site_left.connect(crash_site_right, "Crash Site Left to Right")
 
@@ -51,5 +71,36 @@ def create_and_connect_regions(world: MetroidPrimeOriginsWorld):
     
     _ = gated_east_tallon.connect(life_grove, "Gated East Tallon to Life Grove")
 
+    _ = west_ruins.connect(upper_west_ruins, "West Ruins to Upper West Ruins")
+    _ = west_ruins.connect(central_ruins, "West Ruins to Central Ruins")
+    _ = west_ruins.connect(past_ruins_magma_pool, "West Ruins to Past Magma Pool")
+    _ = west_ruins.connect(ruined_shrine, "West Ruins to Ruined Shrine")
+
+    _ = upper_west_ruins.connect(sun_tower_and_flaahgra, "Upper West Ruins to Flaahgra")
+
+    _ = ruined_shrine.connect(tower_of_light, "Ruined Shrine to Tower of Light")
+
+    _ = central_ruins.connect(past_ruins_magma_pool, "Central Ruins to Past Magma Pool")
+    _ = central_ruins.connect(arboretum, "Central Ruins to Arboretum")
+
+    _ = arboretum.connect(gathering_hall, "Arboretum to Gathering Hall")
+    _ = arboretum.connect(sun_tower_and_flaahgra, "Arboretum to Flaahgra")
+
+    _ = gathering_hall.connect(dynamo, "Gathering Hall to Dynamo")
+    _ = gathering_hall.connect(energy_core, "Gathering Hall to Energy Core")
+
+    _ = energy_core.connect(furnace, "Energy Core to Furnace")
+
+    _ = furnace.connect(upper_furnace, "Furnace to Upper Furnace")
+
+    _ = upper_furnace.connect(crossway, "Upper Furnace to Crossway")
+    _ = upper_furnace.connect(elder_hall, "Upper Furnace to Elder Hall")
+
+    _ = crossway.connect(elder_hall, "Crossway to Elder Hall")
+
+    _ = elder_hall.connect(lower_reflecting_pool, "Elder Hall to Lower Reflecting Pool")
+
+    _ = lower_reflecting_pool.connect(upper_reflecting_pool, "Lower Pool to Upper Pool")
     # Add events
     create_event(inside_frigate, "Open East Tallon Gate")
+    create_event(upper_west_ruins, "Enter Main Plaza From Ledge")
--- apworld/rules.py
+++ apworld/rules.py
@@ -13,13 +13,14 @@ CAN_BOMB = Has("Morph Ball") & Has("Morph Ball Bomb")
 CAN_SUPER_MISSILE = Has("Missile Tank") & Has("Super Missile")
 CAN_BOOST = Has("Morph Ball") & Has("Boost Ball")
 CAN_PB = Has("Morph Ball") & Has("Power Bomb")
-CAN_DESTROY_BOMB_BLOCKS = CAN_BOMB | CAN_PB
+CAN_DESTROY_BOMB_BLOCKS = CAN_BOMB | CAN_PB | Has("Screw Attack")
 
 # Frigate requires shooting conduits, traversing underwater ledges, and destroying bomb blocks + glass to reveal conduits
 CAN_TRAVERSE_FRIGATE = Has("Wave Beam") & CAN_DESTROY_BOMB_BLOCKS & Has("Missile Tank") & (Has("Gravity Suit") | Has("Grapple Beam"))
 
 # This rule should be used for overhangs where you can't swwj but you can get through with vertical movement or climbing up the side
 CAN_TRAVERSE_LOW_OVERHANG = Has("Space Jump Boots") | CAN_SPIDER | Has("Grapple Beam") | CAN_IBJ
+CAN_TRAVERSE_HIGH_OVERHANG = Has("Grapple Beam") | CAN_SPIDER | CAN_IBJ
 
 def set_location_rule(name: str, rule: Rule[Any], world: MetroidPrimeOriginsWorld):
     world.set_rule(world.get_location(name), rule)
@@ -44,6 +45,31 @@ def set_region_connection_rules(world: MetroidPrimeOriginsWorld):
     set_entrance_rule("East Tallon to Gated East Tallon", Has("Open East Tallon Gate"), world)
     set_entrance_rule("Gated East Tallon to Life Grove", CAN_TRAVERSE_LOW_OVERHANG & CAN_PB, world)
     set_entrance_rule("Crash Site Right to Overgrown Cavern", CAN_TRAVERSE_LOW_OVERHANG & Has("Ice Beam"), world)
+    
+    set_entrance_rule("West Ruins to Upper West Ruins", Has("Missile Tank") & Has("Morph Ball"), world)
+    set_entrance_rule("West Ruins to Central Ruins", Has("Morph Ball"), world)
+    set_entrance_rule("West Ruins to Past Magma Pool", Has("Grapple Beam"), world)
+    set_entrance_rule("West Ruins to Ruined Shrine", Has("Missile Tank"), world)
+
+    # Lenient rule
+    set_entrance_rule("Central Ruins to Past Magma Pool", Has("Varia Suit") & Has("Wave Beam") & Has("Grapple Beam"), world)
+    
+    set_entrance_rule("Central Ruins to Arboretum", Has("Missile Tank"), world)
+    set_entrance_rule("Arboretum to Gathering Hall", Has("Missile Tank"), world)
+    set_entrance_rule("Gathering Hall to Dynamo", Has("Missile Tank") & CAN_DESTROY_BOMB_BLOCKS, world)
+    set_entrance_rule("Gathering Hall to Energy Core", Has("Morph Ball"), world)
+
+    set_entrance_rule("Energy Core to Furnace", (Has("Gravity Suit") & CAN_TRAVERSE_LOW_OVERHANG) | Has("Grapple Beam") | CAN_BOMB, world)
+    set_entrance_rule("Furnace to Upper Furnace", Has("Morph Ball") & CAN_DESTROY_BOMB_BLOCKS & (CAN_SPIDER | Has("Grapple Beam")), world)
+    set_entrance_rule("Upper Furnace to Crossway", Has("Morph Ball"), world)
+    set_entrance_rule("Crossway to Elder Hall", CAN_BOOST & ((Has("Ice Beam") & Has("Morph Ball")) | Has("Missile Tank")), world)
+    set_entrance_rule("Upper Furnace to Elder Hall", Has("Ice Beam"), world)
+    set_entrance_rule("Elder Hall to Lower Reflecting Pool", Has("Wave Beam") & CAN_BOMB & CAN_TRAVERSE_LOW_OVERHANG, world)
+    set_entrance_rule("Lower Pool to Upper Pool", (CAN_DESTROY_BOMB_BLOCKS & CAN_BOOST) | Has("Grapple Beam"), world)
+
+    set_entrance_rule("Ruined Shrine to Tower of Light", Has("Wave Beam") & (CAN_SPIDER | Has("Grapple Beam")), world)
+    set_entrance_rule("Arboretum to Flaahgra", Has("Morph Ball") & CAN_DESTROY_BOMB_BLOCKS & Has("Missile Tank"), world)
+    set_entrance_rule("Upper West Ruins to Flaahgra", CAN_SUPER_MISSILE & CAN_TRAVERSE_HIGH_OVERHANG, world)
 
 def set_location_rules(world: MetroidPrimeOriginsWorld):
     # Tallon
@@ -54,12 +80,62 @@ def set_location_rules(world: MetroidPrimeOriginsWorld):
     set_location_rule("(Tallon Overworld) Cargo Freight Lift to Deck Gamma", Has("Missile Tank"), world)
     set_location_rule("(Tallon Overworld) Biohazard Containment", CAN_SUPER_MISSILE, world)
     set_location_rule("(Tallon Overworld) Hydro Access Tunnel", CAN_BOOST, world)
+    set_location_rule("Open East Tallon Gate)", CAN_BOOST, world)
     set_location_rule("(Tallon Overworld) Great Tree Chamber", Has("Screw Attack"), world)
     set_location_rule("(Tallon Overworld) Life Grove Tunnel", CAN_BOOST & CAN_DESTROY_BOMB_BLOCKS, world)
     # Screw through the wall or blow up the secret path
     set_location_rule("(Tallon Overworld) Life Grove - Lake", CAN_BOOST & CAN_DESTROY_BOMB_BLOCKS & (Has("Screw Attack") | CAN_PB), world)
     set_location_rule("(Tallon Overworld) Overgrown Cavern", Has("Morph Ball"), world)
-    
+
+    # Chozo Ruins
+    set_location_rule("(Chozo Ruins) Main Plaza - Super Missile Blocks", CAN_SUPER_MISSILE & CAN_TRAVERSE_LOW_OVERHANG, world)
+    set_location_rule("(Chozo Ruins) Main Plaza - Boost Ball Ramp", CAN_BOOST, world)
+    # Kill the nest and do a low overhang move or just IBJ/grapple
+    set_location_rule("(Chozo Ruins) Main Plaza - Lower Ledge", (Has("Missile Tank") & CAN_TRAVERSE_LOW_OVERHANG) | Has("Grapple Beam") | CAN_IBJ | Has("Enter Main Plaza From Ledge"), world)
+    set_location_rule("(Chozo Ruins) Main Plaza - Top Ledge", Has("Grapple Beam") | CAN_IBJ, world)
+    set_location_rule("(Chozo Ruins) Ruined Nursery", CAN_DESTROY_BOMB_BLOCKS & Has("Morph Ball"), world)
+    set_location_rule("(Chozo Ruins) Ruined Gallery - Right Tunnel", Has("Morph Ball"), world)
+    set_location_rule("(Chozo Ruins) Ruined Gallery - Left Wall", Has("Missile Tank"), world)
+    set_location_rule("(Chozo Ruins) Transport Access North", Has("Missile Tank"), world)
+
+    set_location_rule("(Chozo Ruins) Vault", CAN_BOMB & Has("Morph Ball"), world)
+    set_location_rule("Enter Main Plaza From Ledge", Has("Morph Ball"), world)
+
+    set_location_rule("(Chozo Ruins) Ruined Fountain", CAN_BOOST | CAN_SPIDER, world)
+
+    set_location_rule("(Chozo Ruins) Watery Hall Access", Has("Missile Tank"), world)
+    set_location_rule("(Chozo Ruins) Watery Hall - Top", Has("Missile Tank"), world)
+    set_location_rule("(Chozo Ruins) Watery Hall - Underwater Tunnel", Has("Missile Tank") & Has("Gravity Suit"), world)
+    set_location_rule("(Chozo Ruins) Gathering Hall", Has("Morph Ball") & CAN_DESTROY_BOMB_BLOCKS, world)
+
+    set_location_rule("(Chozo Ruins) Dynamo - Bottom", Has("Missile Tank"), world)
+    set_location_rule("(Chozo Ruins) Dynamo - Top", CAN_TRAVERSE_LOW_OVERHANG, world)
+
+    set_location_rule("(Chozo Ruins) Burn Dome - Fight Reward", Has("Morph Ball"), world)
+    set_location_rule("(Chozo Ruins) Burn Dome - Back Room", Has("Morph Ball") & CAN_DESTROY_BOMB_BLOCKS, world)
+
+    set_location_rule("(Chozo Ruins) Furnace - Chamber", Has("Morph Ball"), world)
+    set_location_rule("(Chozo Ruins) Furnace - Boost Ceiling", CAN_BOOST & CAN_SPIDER & CAN_DESTROY_BOMB_BLOCKS, world)
+
+    set_location_rule("(Chozo Ruins) Crossway", CAN_BOOST & CAN_BOMB, world)
+
+    set_location_rule("(Chozo Ruins) Hall of the Elders - Ceiling", Has("Ice Beam") & CAN_BOMB & CAN_SPIDER, world)
+    # TODO: On remix you can use the secret path
+    set_location_rule("(Chozo Ruins) Elder Chamber", (Has("Plasma Beam") & CAN_BOMB), world)
+    set_location_rule("(Chozo Ruins) Antechamber", Has("Missile Tank") & (CAN_BOMB | CAN_PB), world)
+
+    set_location_rule("(Chozo Ruins) Magma Pool", CAN_PB, world)
+    set_location_rule("(Chozo Ruins) Training Chamber Access", Has("Morph Ball"), world)
+    set_location_rule("(Chozo Ruins) Training Chamber", (CAN_BOOST | CAN_TRAVERSE_LOW_OVERHANG) & CAN_BOMB & Has("Morph Ball"), world)
+
+    set_location_rule("(Chozo Ruins) Ruined Shrine - Ceiling", CAN_BOOST, world)
+    set_location_rule("(Chozo Ruins) Ruined Shrine - Tunnel", CAN_DESTROY_BOMB_BLOCKS & Has("Morph Ball"), world)
+
+    set_location_rule("(Chozo Ruins) Tower of Light", CAN_SUPER_MISSILE, world)
+    set_location_rule("(Chozo Ruins) Tower Chamber", Has("Gravity Suit") & Has("Wave Beam"), world)
+    # Intentionally not CAN_SPIDER since this is about triggering the event flag
+    set_location_rule("(Chozo Ruins) Sunchamber - Ghost Reward", Has("Spider Ball"), world)
+
 
 def set_completion_rule(world: MetroidPrimeOriginsWorld):
     pass
